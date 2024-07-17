e259d80b90e2dc6dca7d4ec4e7503101c7ced2e8
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-07-16 19:50:22 -0500

    auth google

--- index.js
+++ index.js
@@ -7,14 +7,27 @@ const courseRoutes = require("./routes/course.routes");
 const app = express();
 const PORT = process.env.PORT || 4000;
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
-
+const passport = require('passport')
 
 //Usamos express para los middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json()) //Parseador de bodie
+app.use(passport.initialize())
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: 'http://localhost:3000/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000/profile');
+  });
 
 //Aca conectaremos la base de datos
 
