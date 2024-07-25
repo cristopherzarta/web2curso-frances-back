2b98d5ea7f12f81ac94a7972646b951e1903443d
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-07-25 17:06:52 -0500

    cambios WEB NUESTRA

--- passport/index.js
+++ passport/index.js
@@ -37,7 +37,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/auth/google/callback",
+      callbackURL: `${process.env.BASE_BACKEND_URL}/auth/google/callback`,
       passReqToCallback: true,
     },
 
